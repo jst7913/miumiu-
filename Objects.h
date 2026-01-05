776624809a7f7a321c152906348f40b9648033ef
jst7913 <146705747+jst7913@users.noreply.github.com>
2026-01-05 22:23:33 +0000

    Update Objects.h

--- Objects.h
+++ Objects.h
@@ -1,150 +1,115 @@
-#include <iostream>
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
 #include <string>
 
-using namespace std;
+// ================= ENUMS =================
 
 enum class Colour { RED, GREEN, YELLOW };
 enum class Direction { NORTH, EAST, SOUTH, WEST };
 enum class Speed {
-    STOPPED,        // 0 cells/tick
-    HALF_SPEED,     // 1 cell/tick
-    FULL_SPEED      // 2 cells/tick
+    STOPPED,
+    HALF_SPEED,
+    FULL_SPEED
 };
 
-
-
-class GridWorld {
-    int matrix[40][40];
-public:
-    //GridWorld() 
-
-    ~GridWorld () {
-        cout << "[Destructor] Grid World destroyed." << endl;
-    }
-};
-
-//---Objects-----
+// ================= POSITION =================
 
 class Position {
     int x;
     int y;
 public:
-    Position(int x, int y) : x(x), y(y) {}
+    Position(int x = 0, int y = 0);
 
-    ~Position() {}
+    int getX() const;
+    int getY() const;
+    void set(int nx, int ny);
 };
 
+// ================= WORLD OBJECT =================
 
 class WorldObject {
 protected:
-    string id;
+    int id;
     char glyph;
     Position pos;
 
 public:
-    WorldObject(const string& id, char glyph, int x, int y) : id(id), glyph(glyph), pos(x, y){}
+    WorldObject(int id, char glyph, int x, int y);
+    virtual ~WorldObject();
 
-    ~WorldObject() {}
+    int getID() const;
+    char getGlyph() const;
+    Position getPosition() const;
 
-    string getID() const { return id; }
-    char getGlyph() const { return glyph; }
-    Position getPosition() const { return pos; }
-
-    virtual void tick() {}   
+    virtual void tick();
 };
 
-//----- StaticOBJs -----
+// ================= STATIC OBJECTS =================
+
 class StaticObject : public WorldObject {
 public:
-    StaticObject(const string& id, char glyph, int x, int y) : WorldObject(id, glyph, x, y) {}
-
-    ~StaticObject() {}
+    StaticObject(int id, char glyph, int x, int y);
+    virtual ~StaticObject();
 };
 
 class StationaryVehicle : public StaticObject {
-    static int counter;
-
 public:
-    StationaryVehicle(int x, int y) : StaticObject( "PARKED:" + std::to_string(++counter), 'P', x, y) { // ftiaksto analoga to ti zhtaei sth poreia
-        std::cout << "[+PARKED: " << id << "]: Parked at (" << x << ", " << y << ")\n";
-    }
-
-    ~StationaryVehicle() {
-        std::cout << "[-PARKED: " << id << "]: I'm being towed away !\n";
-    }
+    StationaryVehicle(int id, int x, int y);
+    ~StationaryVehicle();
 };
-int StationaryVehicle::counter = 0;
-
 
 class TrafficSign : public StaticObject {
-    static int counter;
-    string text;
-
 public:
-    TrafficSign(int x, int y, const string& text) : StaticObject("SIGN:" + std::to_string(++counter), 'S', x, y), text(text) {
-        //std::cout << "[+" << id << "] Sign created: " << text << " at ("  << x << ", " << y << ")\n";
-    }
-
-    bool isStop() const {
-        return text == "STOP";
-    }
+    TrafficSign(int id, int x, int y);
+    ~TrafficSign();
 };
-int TrafficSign::counter = 0;
-
-
 
 class TrafficLight : public StaticObject {
-private:
-    static int counter;
     Colour color;
     int ticks;
 
 public:
-    TrafficLight(int x, int y) : StaticObject("LIGHT:" + std::to_string(++counter), 'L', x, y), color(Colour::RED), ticks(0) {
-        std::cout << "[+LIGHT: " << id << "] Initialized at (" << x << ", " << y << ") to RED\n";
-    }
+    TrafficLight(int id, int x, int y);
+    ~TrafficLight();
 
-    ~TrafficLight() {
-        std::cout << "[-" << id << "] Turning off\n";
-    }
-
-    void tick() override {
-        ticks++;
-
-        switch (color) {
-        case Colour::RED:
-            if (ticks == 4) { color = Colour::GREEN; ticks = 0; }
-            break;
-        case Colour::GREEN:
-            if (ticks == 8) { color = Colour::YELLOW; ticks = 0; }
-            break;
-        case Colour::YELLOW:
-            if (ticks == 2) { color = Colour::RED; ticks = 0; }
-            break;
-        }
-    }
-
-    Colour getColor() const { return color; }
+    void tick() override;
+    Colour getColor() const;
 };
-int TrafficLight::counter = 0;
 
+// ================= MOVING OBJECT =================
 
-//----- MovingOBJs -----
 class MovingObject : public WorldObject {
 protected:
     Direction dir;
     Speed speed;
 
 public:
-    MovingObject(const std::string& id, char glyph, int x, int y, Direction dir) : WorldObject(id, glyph, x, y), dir(dir), speed(Speed::STOPPED) {}
+    MovingObject(int id, char glyph, int x, int y, Direction d, Speed s);
+    virtual ~MovingObject();
 
-    ~MovingObject() {}
+    Speed getSpeed() const;
+    Direction getDirection() const;
 
-    virtual void tick() = 0;
+    void tick() override;
+};
+
+// ================= VEHICLES =================
+
+class Car : public MovingObject {
+public:
+    Car(int id, int x, int y, Direction d);
+    ~Car();
+};
+
+class Bike : public MovingObject {
+public:
+    Bike(int id, int x, int y, Direction d);
+    ~Bike();
 };
 
 
+const char* dirToStr(Direction d);
+const char* speedToStr(Speed s);
 
-
-
-
+#endif // OBJECTS_H
